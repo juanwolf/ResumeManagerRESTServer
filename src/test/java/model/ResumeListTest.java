38f47d20e5ebdc2e735e74b64b14ee0fca26c71c
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-04-10 00:39:11 +0200

    Adding unit test for the ResumeList class.

--- src/test/java/model/ResumeListTest.java
+++ src/test/java/model/ResumeListTest.java
@@ -1,7 +1,49 @@
 package model;
 
-/**
- * Created by juanwolf on 09/04/14.
- */
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResumeListTest {
+
+    @Test
+    public void EmptyConstructorCreateAResumeListWithNoList() {
+        // Given
+        // When
+        ResumeList resumeList = new ResumeList();
+        // Then
+        Assertions.assertThat(resumeList.getResumeList())
+                .isNull();
+
+    }
+
+    @Test
+    public void SecondConstructorCreateAResumeListWithAnResumeListInitialized() {
+        // Given
+        List<Resume> resumes = new ArrayList<Resume>();
+        resumes.add(new Resume());
+        // When
+        ResumeList resumeList = new ResumeList(resumes);
+        // Then
+        Assertions.assertThat(resumeList.getResumeList()).isEqualTo(resumes);
+    }
+
+    @Test
+    public void getResumeByIdMustReturnTheCorrectResume() {
+        // Given
+        String resumeName = "Test";
+        List<Resume> resumes = new ArrayList<Resume>();
+        Resume resume = new Resume(resumeName, null, null, null, null, null, null);
+        int id = resume.getId();
+        resumes.add(resume);
+        resumes.add(new Resume());
+        ResumeList resumeList = new ResumeList(resumes);
+        // When
+        Resume testResume = resumeList.getResumeById(id);
+        // Then
+        Assertions.assertThat(testResume).isEqualTo(resume);
+    }
 }
